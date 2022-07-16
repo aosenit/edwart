fced2b800194a02483d3825e8c1100f84438ead3
oseni ade <oseniadedoyin22@gmail.com>
2022-07-16 12:53:15 +0100

    initialization

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,30 @@
+import "./styles.scss";
+
+const Header = () => {
+  return (
+    <section>
+      <header>
+        <section className="logo">
+          <img src="" alt="" />
+          <h3>Edwart</h3>
+        </section>
+        <nav className="headerNav">
+          <ul>
+            <li>Home</li>
+            <li>Facilities</li>
+            <li>Experience</li>
+            <li>Community Relations plan</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+
+        <section className="menu">=</section>
+      </header>
+      <section className="hero" id="home">
+        Hero
+      </section>
+    </section>
+  );
+};
+
+export default Header;
